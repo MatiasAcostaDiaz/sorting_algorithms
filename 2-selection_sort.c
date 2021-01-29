8aa335b62ed38323cada4b624a429b2e836bd335
Matias Acosta <2277@holbertonschool.com>
2021-01-29 22:04:10 +0000

    Some

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
-#include <sort.h>
+#include "sort.h"
 
 /**
- * selection_sort - 
+ * selection_sort - order an array using the algorithm selection_sort 
  * @array: the array
  * @size: the size of the array
  * Return: Nothing
@@ -9,5 +9,31 @@
 
 void selection_sort(int *array, size_t size)
 {
+	size_t i = 0, j = 0, k = 0;
+	int tmp = 0, dancer = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		dancer = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (dancer > array[j])
+			{
+				tmp = array[j];
+				array[j] = dancer;
+				dancer = tmp;
+				for (k = j; k < size - 1; k++)
+				{
+					if (dancer > array[k + 1])
+					{
+						tmp = array[k + 1];
+						array[k + 1] = dancer;
+						dancer = tmp;
+					}
+				}
+			}
+		}
+		array[i] = dancer;
+		print_array(array, size);
+	}
 }
