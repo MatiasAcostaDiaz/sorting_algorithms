51c677356a70e007d6f2218b19ed9e29dde3b2af
Matias Acosta <2277@holbertonschool.com>
2021-01-29 14:22:09 +0000

    Ex 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,26 +11,19 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp = 0, count = 0; 
 	size_t i = 0;
-
 	while (1)
 	{
-		count = 0;
+		count =0;
 		for (i = 0; i < size; i++)
 		{
-			if ((i + 1) < size)
+			while (array[i] > array[i + 1] && i + 1 < size)
 			{
-				if (array[i] > array[i + 1] && count == 0)
-				{
-					temp = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = temp;
-					count = 1;
-				}
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				count++;
 			}
-			if (i + 1 == size)
-				printf("%d\n", array[i]);
-			else
-				printf("%d, ", array[i]);
 		}
 		if (count == 0)
 			break;
