eebfaaeb12b08f8e148f5237a1c5aa3d996e5b3f
Matias Acosta <2277@holbertonschool.com>
2021-02-01 15:10:08 -0300

    Fix 1

--- sort.h
+++ sort.h
@@ -28,4 +28,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B);
+void print_list(const listint_t *list);
 #endif
