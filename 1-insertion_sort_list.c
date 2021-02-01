27338ff99ed8c2e838baf4b4c0a9376d8a65666c
Matias Acosta <2277@holbertonschool.com>
2021-02-01 14:39:01 -0300

    Fixing some tasks

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,95 @@
-#include <sort.h>
+#include "sort.h"
 
 /**
- * insertion_sort_list -
- * @list:
+ * insertion_sort_list - order a double linked
+ * list using the algorithm insertion_sort
+ * @list: Double linked list
  * Return: Nothing
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *aux = NULL, *temp = NULL;
+	listint_t *aux = NULL, *temp = NULL, *temp2 = NULL;
+
+	if (!list || !(*list) || (!(*list)->next))
+		return;
+
+	aux = *list;
+	while (aux)
+	{
+		if (aux->prev)
+		{
+			if (aux->prev->n > aux->n)
+			{
+				swap_nodes(list, aux->prev, aux);
+				print_list(*list);
+				temp = aux;
+				temp2 = aux;
+				aux = aux->next;
+
+				while (temp2)
+				{
+					if (temp2->n > temp->n)
+					{
+						swap_nodes(list, temp2, temp);
+						print_list(*list);
+					}
+					temp2 = temp2->prev;
+				}
+			}
+		}
+		aux = aux->next;
+	}
+}
+
+/**
+ * swap_nodes - swap the nodes of the list
+ * @list: Double linked list
+ * @node_A: Node A
+ * @node_B: Node B
+ * Return: Nothing
+ */
+
+void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
+{
+	node_A->prev = node_B;
+	node_B->next = node_A;
+	if (node_A->prev == NULL && node_B->next == NULL)
+	{
+		node_A->next = NULL;
+		node_B->prev = NULL;
+		*list = node_B;
+		return;
+	}
+	if (node_A->prev == NULL)
+	{
+		listint_t *t1 = node_B->next;
+
+		node_A->next = t1;
+		node_B->prev = NULL;
+		t1->prev = node_A;
+		*list = node_B;
+		return;
+	}
+
+	if (node_B->next == NULL)
+	{
+		listint_t *t1 = node_A->prev;
+
+		node_B->prev = t1;
+		t1->next = node_B;
+		node_A->next = NULL;
+		return;
+	}
+	else
+	{
+		listint_t *t1 = node_A->prev;
+		listint_t *t2 = node_B->next;
+
+		node_A->next = t2;
+		node_B->prev = t1;
+		t1->next = node_B;
+		t2->prev = node_A;
+		return;
+	}
 }
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,4 +1,4 @@
-#include <sort.h>
+#include "sort.h"
 
 /**
  * counting_sort -
@@ -7,7 +7,46 @@
  * Return: Nothing
  */
 
+int largest_int(int *array, size_t size)
+{
+    size_t i = 0;
+    int largest = 0;
+
+    if (array == NULL || size == 0)
+        return (0); 
+    largest = array[0];
+    for(; i < size; i++)
+    {
+        if (largest < array[i])
+            largest = array[i];
+    }
+
+    return (largest);
+}
+
+int is_in(int *array, size_t size, int num)
+{
+    size_t i = 0;
+    int count = 0;
+
+    
+
+    return (count);
+}
+
 void counting_sort(int *array, size_t size)
 {
+    int largest = 0;
 
+    largest = largest_int(array, size);
+    printf("El numero maximo es %d\n", largest);
+}
+
+int main(void)
+{
+     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    counting_sort(array, n);
+    return (0);
 }
