9dd02160f14e2595c9610b3821bc1233f5eb2bd1
Matias Acosta <2277@holbertonschool.com>
2021-02-01 18:14:16 -0300

    Add Functions of quick sort

--- sort.h
+++ sort.h
@@ -32,4 +32,7 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B);
 void print_list(const listint_t *list);
 int largest_int(int *array, size_t size);
 int is_in(int *array, size_t size, int num);
+int partition(int *array, ssize_t start, ssize_t next, size_t size);
+void swap(int *a, int *b);
+void quicksort(int *array, ssize_t start, ssize_t next, size_t size);
 #endif
